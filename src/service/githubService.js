36d52cc7f2446bda1bf2df1554a54f4e4c5da8d7
ed <ed@edmail.edcom>
2026-07-11 08:14:07 +0300

    Added fetch service from GitHub

---
+++ src/service/githubService.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://api.github.com/';
+
+export async function fetchGithubProfile(username){
+  const [profileRes, repoRes] = await Promise.all([
+    fetch(`${BASE_URL}/users/${username}`),
+    fetch(`${BASE_URL}/users/${username}/repos/per_page=100&sort=updated`),
+  ])
+
+  if(profileRes.status === 404)
+    throw new Error(`Username ${username} not found on GitHub!`);
+
+  if(!profileRes.ok || !repoRes.ok)
+    throw new Error(`Failed to fetch either profile or repos from github`);
+
+  const profile = await profileRes.json();
+  const repos = await repoRes.json();
+
+  return { profile, repos };
+}
